77c2d0e4c3fd87c1f45ce8fc4f3f2ea9c1e49166
Laura Hild <lshild@wm.edu>
2015-01-28 13:25:00 -0500

    Reorganize Puppet manifest

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,35 +1,65 @@
-# http://stackoverflow.com/questions/10845864
-exec { 'apt-update':
-  command => "/usr/bin/apt-get update",
-  onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
+Exec {
+  path => '/usr/sbin:/usr/bin:/sbin:/bin',
 }
-Exec['apt-update'] -> Package <| |>
 
 package { [
-    'libapache2-mod-php5',
-    'php-apc',
     'default-jdk',
+    'libapache2-mod-php5',
     'php5',
     'php5-curl', # needed for CAS
     'php5-dev',
-    'php-pear',
+    'php5-gd',
+    'php5-intl',
     'php5-json',
     'php5-ldap',
     'php5-mcrypt',
     'php5-mysql',
+    'php5-xdebug',
     'php5-xsl',
-    'php5-intl',
-    'php5-gd',
+    'php-apc',
+    'php-pear',
   ]:
   ensure => installed,
 }
 
-class { '::mysql::server': root_password => 'UNSET', }
+# Avoid package installation errors by running 'apt-get update' before
+# attempting to install any packages if the APT cache has not been modified
+# in over a week (inspired by http://stackoverflow.com/a/14754159):
+exec { 'apt-update':
+  command => 'apt-get update',
+  onlyif => "expr `date +%s` - `stat -c%Y /var/cache/apt/` '>' 604800"
+}
+Exec['apt-update'] -> Package <| |>
+
+# Other modules are enabled when they are installed,
+# but Ubuntu's php5-mcrypt is not.
+exec { 'php5enmod mcrypt':
+  creates => '/etc/php5/apache2/conf.d/20-mcrypt.ini',
+  notify => Service['apache2'],
+  require => Package['php5-mcrypt'],
+}
+
+# This is a development setup, so we want xdebug available
+# to be enabled, but the module can have a significant impact
+# on performance when enabled, so disable it when provisioning:
+exec { 'php5dismod xdebug':
+  require => Package['php5-xdebug'],
+  notify => Service['apache2'],
+  onlyif => 'php5query -s apache2 -m xdebug || php5query -s cli -m xdebug',
+}
+
+##### MySQL setup #############################################################
+
+class { '::mysql::server':
+  root_password => 'UNSET',
+}
+
+##### Apache setup ############################################################
 
 service { 'apache2':
   ensure => 'running',
   enable => 'true',
-  require => Package['libapache2-mod-php5']
+  require => Package['libapache2-mod-php5'],
 }
 
 file { '/etc/apache2/conf-enabled/vufind.conf':
@@ -43,6 +73,8 @@ file { '/etc/apache2/mods-enabled/rewrite.load':
   ensure => 'link',
   target => '/etc/apache2/mods-available/rewrite.load',
 }
+
+##### Solr setup ##############################################################
 
 file { '/usr/local/vufind2':
   ensure => 'link',
@@ -60,24 +92,8 @@ service { 'vufind':
   require => [
     File['/etc/init.d/vufind'],
     File['/usr/local/vufind2'],
+    Package['default-jdk'],
   ],
   hasstatus => false,
   status => 'sudo /etc/init.d/vufind check'
-}
-
-exec { '/usr/sbin/php5enmod mcrypt':
-  creates => '/etc/php5/apache2/conf.d/20-mcrypt.ini',
-  notify => Service['apache2'],
-  require => Package['php5-mcrypt'],
-}
-
-# This is a development setup, so we want xdebug available to be enabled,
-# but the module can have a significant impact on performance when enabled,
-# so disable it.
-package { 'php5-xdebug': ensure => 'installed' }
-exec { '/usr/sbin/php5dismod xdebug':
-  require => Package['php5-xdebug'],
-  notify => Service['apache2'],
-  onlyif => "/usr/sbin/php5query -s apache2 -m xdebug ||\
-             /usr/sbin/php5query -s cli -m xdebug",
 }
